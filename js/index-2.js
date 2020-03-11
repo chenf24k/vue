e1760c03092c0b1324e50701ecbbfcf4cf578985
chenf24k <chenf24k@163.com>
2020-03-11 16:02:50 +0800

    不懂啊

---
+++ js/index-2.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+	let app = new Vue({
+		el: '#watch-example',
+		data: {
+			question: '',
+			answer: 'I cannot give you an answer until you ask a question!',
+		},
+		// 如果 `question` 发生改变，这个函数就会运行
+		watch: {
+			question: function(newQst, OldQst) {
+				this.answer = 'Waiting for you to stop typing...'
+				this.debouncedGetAnswer()
+			}
+		},
+		created: function() {
+			this.debouncedGetAnswer = _.debounce(this.getAnswer, 500)
+		},
+		methods: {
+			getAnswer: function() {
+				if (this.question.indexOf('?') === -1) {
+					this.answer = 'Questions usually contain a question mark. ;-)'
+					return;
+				}
+				this.answer = 'Thinking...'
+				var vm = this
+				axios.get('https://yesno.wtf/api')
+				.then(function(response){
+					vm.answer = _.capitalize(response.data.answer)
+				})
+				.catch(function(error){
+					vm.answer = 'Error! Could not reach the API.'+ error
+				})
+			}
+		}
+	});
+}
--- learn/README.md
+++ learn/README.md
@@ -1,3 +1,21 @@
-1.声明式渲染
-  Vue.js 的核心是一个允许采用简洁的模板语法来声明式地将数据渲染进 DOM 的系统
-2.指令
+1. 声明式渲染
+   Vue.js 的核心是一个允许采用简洁的模板语法来声明式地将数据渲染进 DOM 的系统
+2. Mustache 语法： {{ msg }}
+
+3. Mustache 语法不能作用在 HTML attribute 上
+   应使用 v-bind 指令，可以用于响应式地更新 HTML attribute
+   <a v-bind:href="url">...</a>
+   <a v-on:click="doSomething">...</a>
+
+4. 动态参数
+   <a v-bind:[attributeName]="url"> ... </a>
+   <a v-on:[eventName]="doSomething"> ... </a>
+   
+5. 缩写
+   v-bind
+   <a :href="url">...</a>
+   <a :[key]="url"> ... </a>
+   v-on
+   <a @click="doSomething">...</a>
+   <a @[event]="doSomething"> ... </a>
+   
